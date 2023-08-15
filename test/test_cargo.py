2177d21092020541d99ca52dac7503c165d065c5
Flor Velo Bares <florenciavelobares@gmail.com>
2023-08-15 16:47:16 -0300

    ultimos cambios

---
+++ test/test_cargo.py
@@ -0,0 +1,18 @@
+import pytest 
+from src.Cargo import Cargo
+
+def test_Cargo_Mayor20() -> None:
+    
+    cargoA= Cargo(10,1,100,5) #100-(5*1.5+3.5*10)=57.5
+    assert cargoA.is_worth_it() == 57.5
+    assert not cargoA.is_worth_it() == 890
+    
+    cargoB= Cargo(15,2,65,2) #65-(2*1.5+3.5*15)= 27
+    assert cargoB.is_worth_it() == 27
+    assert not cargoB.is_worth_it() == 876
+    
+
+def test_Cargo_Menor20() -> None:
+    cargoC= Cargo(10,1,50,9) #50-(9*1.5+3.5*10)=  1.7
+    with pytest.raises(ValueError, "¡TRIPULACION BAJE LA GUARDIA! No hay tal botin:,)"):
+        cargoC.is_worth_it()
---
+++ test/test_cruise.py
@@ -0,0 +1,18 @@
+import pytest 
+from src.Cruise import Cruise
+
+def test_Cruise_Mayor20() -> None:
+    
+    cruceroA= Cruise(230,2000,35) #2000 -(230*2.25 + 35*1,5) = 1430
+    assert cruceroA.is_worth_it() == 1430
+    assert not cruceroA.is_worth_it() == 130
+    
+    cruceroB= Cruise(20,1000,65) #1000 -(20*2.25 + 65*1,5) = 1000 - 142,5 = 857,5
+    assert cruceroB.is_worth_it() == 857.5
+    assert not cruceroB.is_worth_it() == 10
+    
+
+def test_Cruise_Menor20() -> None:
+    cruceroC= Cruise(20,150,65) #150 -(20*2.25 + 65*1,5) = 150 - 142,5 = 7,5
+    with pytest.raises(ValueError, "¡TRIPULACION BAJE LA GUARDIA! No hay tal botin:,)"):
+        cruceroC.is_worth_it()    
