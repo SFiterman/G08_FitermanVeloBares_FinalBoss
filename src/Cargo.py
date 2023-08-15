7ac62c7286e82c9f5697fb657e023dc5e2abc66e
Flor Velo Bares <florenciavelobares@gmail.com>
2023-08-15 15:57:26 -0300

    main + creacion tests

--- src/Cargo.py
+++ src/Cargo.py
@@ -1,4 +1,5 @@
 from Ship import Ship
+
 class Cargo (Ship):
  def __init__(self, cargo, quality, draft, crew): #constructor.
     self.cargo = cargo
@@ -16,11 +17,12 @@ def calcularPeso(self): #VIRTUAL HIJO
    auxCargo=0
 
    if(self.quality==1):
-      auxCargo=+3.5
+      auxCargo=3.5
    elif(self.quality==0.5):
-    auxCargo=+2
+    auxCargo=2
    elif(self.quality==0.25):
-    auxCargo=+0.5
+    auxCargo=0.5
    
+   auxCargo = auxCargo*self.quality #asumimos que todos los cargos son del mismo peso por como esta presentado en el archivo csv
    aux= self.draft - (self.crew* Ship.pesotripulacion + auxCargo)
    return aux
